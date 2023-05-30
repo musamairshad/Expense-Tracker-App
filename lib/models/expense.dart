82aedf99327abfa9dafd393b9293bb877205f32b
Muhammad Usama <imusamairshad@gmail.com>
2023-05-30 22:32:33 +0500

    expense model is modified.

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,12 +2,19 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
+enum Category { food, travel, leisure, work }
+
 class Expense {
-  Expense({required this.title, required this.amount, required this.date})
-      : id = uuid.v4();
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4();
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }
