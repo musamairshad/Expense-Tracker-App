c115f84768757d0a8463fe7147a7f3359ee68ec9
Muhammad Usama <imusamairshad@gmail.com>
2023-06-01 20:57:46 +0500

    expenses_list.dart file is modified.

---
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker_app/models/expense.dart';
+import 'package:expense_tracker_app/widgets/expenses_list/expense_item.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({super.key, required this.expenses});
+
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (ctx, index) => ExpenseItem(expenses[index]),
+    );
+  }
+}
