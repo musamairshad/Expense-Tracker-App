7822e03c9fa84ceb6a41c8d0de1531eacf16084c
Muhammad Usama <imusamairshad@gmail.com>
2023-06-01 20:56:06 +0500

    expense_item.dart file is added.

---
+++ lib/widgets/expenses_list/expense_item.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker_app/models/expense.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem(this.expense, {super.key});
+
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 16.0),
+        child: Column(
+          children: [
+            Text(expense.title),
+            const SizedBox(height: 4.0),
+            Row(
+              children: [
+                Text(
+                  '\$${expense.amount.toStringAsFixed(2)}', // eg: 12.3433 => 12.34
+                ),
+                const Spacer(),
+                Row(
+                  children: [
+                    const Icon(Icons.alarm),
+                    const SizedBox(width: 8.0),
+                    Text(
+                      expense.date.toString(),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
