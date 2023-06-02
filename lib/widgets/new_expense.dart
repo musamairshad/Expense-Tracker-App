bfa93d449b0777dd5cc77b2aa058b5be2ff95000
Muhammad Usama <imusamairshad@gmail.com>
2023-06-02 15:40:57 +0500

    new_expense.dart file is modified.

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -9,17 +9,34 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  var _enteredTitle = '';
+
+  void _saveTitleInput(String inputValue) {
+    _enteredTitle = inputValue;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16.0),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
       child: Column(
         children: [
           TextField(
+            onChanged: _saveTitleInput,
             maxLength: 50,
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               label: Text('Title'),
             ),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  // print(_enteredTitle);
+                },
+                child: const Text('Save Expense'),
+              ),
+            ],
           ),
         ],
       ),
