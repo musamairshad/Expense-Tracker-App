7960b87397d0309b8c37f05fce57279ec14a6b99
Muhammad Usama <imusamairshad@gmail.com>
2023-06-05 23:10:40 +0500

    expenses.dart is modified.

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -29,6 +29,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+      isScrollControlled: true,
       context: context,
       builder: (ctx) => NewExpense(onAddExpense: _addExpense),
     );
@@ -37,6 +38,12 @@ class _ExpensesState extends State<Expenses> {
   void _addExpense(Expense expense) {
     setState(() {
       _registeredExpenses.add(expense);
+    });
+  }
+
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
     });
   }
 
@@ -56,7 +63,10 @@ class _ExpensesState extends State<Expenses> {
         children: [
           const Text('The Chart!'),
           Expanded(
-            child: ExpensesList(expenses: _registeredExpenses),
+            child: ExpensesList(
+              expenses: _registeredExpenses,
+              onRemoveExpense: _removeExpense,
+            ),
           ),
         ],
       ),
