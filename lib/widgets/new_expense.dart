007cc5b73b5bd5b31e2b317b1fd589d9540b87da
Muhammad Usama <imusamairshad@gmail.com>
2023-06-05 19:12:56 +0500

    new_expense.dart is modified.

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -34,6 +34,16 @@ class _NewExpenseState extends State<NewExpense> {
     });
   }
 
+  void _submitExpenseData() {
+    final enteredAmount = double.tryParse(_amountController.text);
+    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
+    if (_titleController.text.trim().isEmpty ||
+        amountIsInvalid ||
+        _selectedDate == null) {
+      // show error message...
+    }
+  }
+
   @override
   void dispose() {
     _titleController.dispose();
@@ -118,10 +128,7 @@ class _NewExpenseState extends State<NewExpense> {
                 child: const Text('Cancel'),
               ),
               ElevatedButton(
-                onPressed: () {
-                  // print(_titleController.text);
-                  // print(_amountController.text);
-                },
+                onPressed: _submitExpenseData,
                 child: const Text('Save Expense'),
               ),
             ],
