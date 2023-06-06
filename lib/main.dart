5534966332374ac9a65ce34af06513e9b7706fe5
Muhammad Usama <imusamairshad@gmail.com>
2023-06-06 23:07:43 +0500

    main.dart is modified.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker_app/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        // scaffoldBackgroundColor: const Color.fromARGB(255, 220, 189, 252),
+        // elevatedButtonTheme: ElevatedButtonThemeData(),
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
       home: const Expenses(),
     ),
   );
