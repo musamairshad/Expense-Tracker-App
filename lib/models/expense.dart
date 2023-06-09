bc5319ca0f59d967308bb7262384fe0c6747893e
Muhammad Usama <imusamairshad@gmail.com>
2023-06-09 20:02:53 +0500

    expense.dart is modified.

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -33,3 +33,20 @@ class Expense {
     return formatter.format(date);
   }
 }
+
+class ExpenseBucket {
+  const ExpenseBucket({required this.category, required this.expenses});
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+
+    return sum;
+  }
+}
