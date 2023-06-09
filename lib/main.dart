9772ae411f21581157f1a640933c436dfb347032
Muhammad Usama <imusamairshad@gmail.com>
2023-06-09 17:18:37 +0500

    main.dart is modified.

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,29 @@ var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
 );
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
+
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
       theme: ThemeData().copyWith(
         useMaterial3: true,
         // scaffoldBackgroundColor: const Color.fromARGB(255, 220, 189, 252),
@@ -35,6 +54,7 @@ void main() {
               ),
             ),
       ),
+      // themeMode: ThemeMode.system, // default
       home: const Expenses(),
     ),
   );
