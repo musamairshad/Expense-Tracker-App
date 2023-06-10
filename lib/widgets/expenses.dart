82aadf81fbf6a0c0753484ed0b4a98f1b7e6ccc8
Muhammad Usama <imusamairshad@gmail.com>
2023-06-10 12:24:24 +0500

    expenses.dart is modified.

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker_app/models/expense.dart';
 import 'package:expense_tracker_app/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker_app/widgets/new_expense.dart';
+import 'package:expense_tracker_app/widgets/chart/chart.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -88,7 +89,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          const Text('The Chart!'),
+          Chart(expenses: _registeredExpenses),
           Expanded(
             child: mainContent,
           ),
