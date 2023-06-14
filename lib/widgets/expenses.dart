89d253f68fb6a9328b8a88a082d3d844151e96d4
Muhammad Usama <imusamairshad@gmail.com>
2023-06-14 19:19:01 +0500

    expenses.dart is modified.

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -30,6 +30,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+      useSafeArea: true,
       isScrollControlled: true,
       context: context,
       builder: (ctx) => NewExpense(onAddExpense: _addExpense),
